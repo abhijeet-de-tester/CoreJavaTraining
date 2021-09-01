c7abaeb050c9fd79974c5274ac00dcfc46b24fd9
Abhijeet Deshpande <41492779+abhijeet-de-tester@users.noreply.github.com>
2021-09-01 20:14:31 +0530

    Update StringReverse.java
    added code written by me on HackerRank site.

--- StringReverse.java
+++ StringReverse.java
@@ -1,1 +1,27 @@
+import java.io.*;
+import java.util.*;
 
+public class StringReverse {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        byte[] revA = new byte[A.length()];
+        byte[] bytesA = A.getBytes();
+        for(int i=0; i<A.length(); i++){
+            revA[i] = bytesA[A.length()-1-i];
+        }
+        
+        String strRevA = new String(revA);
+        //System.out.println(strRevA);
+        if (strRevA.compareTo(A)==0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+        
+    }
+}
